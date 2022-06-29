8598b0ed5da992e67a1997ed9f3386c381d75e33
valencarballo <valentincarballo99@gmail.com>
2022-06-29 11:48:20 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer greeting={"Entregable"}/>
+      <ItemListContainer greeting={"Visualizacion de inicio"}/>
     </div>
   );
 }
